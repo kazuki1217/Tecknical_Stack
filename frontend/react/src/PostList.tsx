1fb7648ddb41b4631971a14aa3e85066dfe12b37
kazuki1217 <kazuki1217skisaikou@gmail.com>
2025-06-24 00:20:59 +0900

    ログアウト機能を追加

--- frontend/react/src/PostList.tsx
+++ frontend/react/src/PostList.tsx
@@ -1,6 +1,22 @@
 // ログイン認証後の画面
+
+import { useNavigate } from 'react-router-dom';
+
 function PostList({ user }: { user: string }) {
-  return <h1>こんにちは「{user}」さん</h1>;
-}
+  const navigate = useNavigate();
+
+  // ログアウト処理
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+    window.location.reload(); // App.tsx を再評価させて状態をリセット
+  };
+
+  return (
+    <div>
+      <h1>こんにちは「{user}」さん</h1>
+      <button onClick={handleLogout}>ログアウト</button>
+    </div>
+  );}
 
 export default PostList;
