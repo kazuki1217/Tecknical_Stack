2547c12a586c3ad366709f337ce2777d367465f6
kazuki1217 <kazuki1217skisaikou@gmail.com>
2025-06-29 22:51:50 +0900

    refactor(api): 同じ役割を果たすルートが２つ存在するため、片方を削除。不要な認証判定処理を削除。

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -4,16 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
-// アカウント登録（Reactでは、アカウント登録情報を入力した後に使う）
+// アカウント登録処理
 Route::post('/register', [AuthController::class, 'register']);
 
-// ログイン認証処理（Reactでは、ログイン情報入力後に使う）
+// ログイン認証処理
 Route::post('/login', [AuthController::class, 'login']);
 
-// ログイン状態の確認
+// 有効なトークンであるか確認
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'loginSuccess']); // ユーザー名を取得
     Route::get('/posts', [PostController::class, 'index']); // 投稿一覧を取得
     Route::post('/posts', [PostController::class, 'store']); // 投稿内容を保存
-    Route::get('/user', [AuthController::class, 'loginSuccess']); // ユーザー情報の取得（Reactでは、ベースURLアクセス時に使う）
-    Route::get('/loginsuccess', [AuthController::class, 'loginSuccess']); // ユーザー情報の取得（Reactでは、ログイン情報入力後に使う）
 });
