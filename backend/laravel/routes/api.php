0669e1a5f19ea33fc6616d46a439a992d2ed5b2f
kazuki1217 <kazuki1217skisaikou@gmail.com>
2025-06-29 17:49:01 +0900

    refactor(api):  不要なルートを削除

--- backend/laravel/routes/api.php
+++ backend/laravel/routes/api.php
@@ -1,30 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
 
-
-Route::get('/message', function () {
-    return response()->json(['message' => 'Hello from Laravel!']);
-});
-
 // アカウント登録（Reactでは、アカウント登録情報を入力した後に使う）
 Route::post('/register', [AuthController::class, 'register']);
 
-// ログイン状態の確認とユーザー情報の取得（Reactでは、ベースURLアクセス時に使う）
-Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'loginSuccess']);
-
 // ログイン認証処理（Reactでは、ログイン情報入力後に使う）
 Route::post('/login', [AuthController::class, 'login']);
-
-// ログイン状態の確認とユーザー情報の取得（Reactでは、ログイン情報入力後に使う）
-Route::middleware('auth:sanctum')->get('/loginsuccess', [AuthController::class, 'loginSuccess']);
 
 // ログイン状態の確認
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/posts', [PostController::class, 'index']); // 投稿一覧を取得
     Route::post('/posts', [PostController::class, 'store']); // 投稿内容を保存
+    Route::get('/user', [AuthController::class, 'loginSuccess']); // ユーザー情報の取得（Reactでは、ベースURLアクセス時に使う）
+    Route::get('/loginsuccess', [AuthController::class, 'loginSuccess']); // ユーザー情報の取得（Reactでは、ログイン情報入力後に使う）
 });
 
