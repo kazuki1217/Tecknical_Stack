6d2442e4125d7d3a81672d8317ad6e79bbc609b5
kazuki_nukui <kazuki.nukui@g.dreamcareer.co.jp>
2025-07-16 16:47:10 +0900

    fix: 新規登録画面、ログイン画面の UI を修正

--- frontend/react/src/components/Login.tsx
+++ frontend/react/src/components/Login.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/index.css";
 
 interface LoginProps {
   setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>;
--- frontend/react/src/components/Register.tsx
+++ frontend/react/src/components/Register.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../styles/index.css";
 
 /** 新規登録画面を構成 */
 function Register() {
